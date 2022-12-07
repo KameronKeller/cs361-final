8d92801e1e2f284d7bef2251e89265f2f5c96d83
Kameron Keller <mrkameronkeller@gmail.com>
2022-12-06 21:59:21 -0800

    Autoformat

--- tc_gis.rb
+++ tc_gis.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'test/unit'
 
 class TestGis < Test::Unit::TestCase
-
   def test_waypoints
     point = Point.new(-121.5, 45.5, 30)
     w = Waypoint.new(point, "home", "flag")
@@ -26,17 +25,17 @@ class TestGis < Test::Unit::TestCase
 
   def test_tracks
     ts1 = TrackSegment.new([
-      Point.new(-122, 45),
-      Point.new(-122, 46),
-      Point.new(-121, 46),
-    ])
+                             Point.new(-122, 45),
+                             Point.new(-122, 46),
+                             Point.new(-121, 46),
+                           ])
 
-    ts2 = TrackSegment.new([ Point.new(-121, 45), Point.new(-121, 46), ])
+    ts2 = TrackSegment.new([Point.new(-121, 45), Point.new(-121, 46),])
 
     ts3 = TrackSegment.new([
-      Point.new(-121, 45.5),
-      Point.new(-122, 45.5),
-    ])
+                             Point.new(-121, 45.5),
+                             Point.new(-122, 45.5),
+                           ])
 
     t = Track.new([ts1, ts2], "track 1")
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}}')
@@ -55,18 +54,17 @@ class TestGis < Test::Unit::TestCase
     p2 = Point.new(-121.5, 45.6, nil)
     w2 = Waypoint.new(p2, "store", "dot")
     ts1 = TrackSegment.new([
-      Point.new(-122, 45),
-      Point.new(-122, 46),
-      Point.new(-121, 46),
-    ])
+                             Point.new(-122, 45),
+                             Point.new(-122, 46),
+                             Point.new(-121, 46),
+                           ])
 
-
-    ts2 = TrackSegment.new([ Point.new(-121, 45), Point.new(-121, 46), ])
+    ts2 = TrackSegment.new([Point.new(-121, 45), Point.new(-121, 46),])
 
     ts3 = TrackSegment.new([
-      Point.new(-121, 45.5),
-      Point.new(-122, 45.5),
-    ])
+                             Point.new(-121, 45.5),
+                             Point.new(-122, 45.5),
+                           ])
 
     t = Track.new([ts1, ts2], "track 1")
     t2 = Track.new([ts3], "track 2")
@@ -77,5 +75,4 @@ class TestGis < Test::Unit::TestCase
     result = JSON.parse(w.to_geojson)
     assert_equal(expected, result)
   end
-
 end
